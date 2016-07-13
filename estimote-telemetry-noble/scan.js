33ff69ffdc37d4646027b8f54803839dc74f6c88
Piotr Krawiec <piotr@krawiec.me>
2016-07-13 12:26:09 +0200

    Example use of `noble` to scan Estimote Telemetry

---
+++ estimote-telemetry-noble/scan.js
@@ -0,0 +1,55 @@
+var noble = require('noble');
+
+var ESTIMOTE_FRAME_TYPE_TELEMETRY = 2;
+
+var ESTIMOTE_TELEMETRY_SUBFRAME_A = 0;
+var ESTIMOTE_TELEMETRY_SUBFRAME_B = 1;
+
+noble.on('stateChange', function(state) {
+  console.log('state has changed', state);
+  if (state == 'poweredOn') {
+    var serviceUUIDs = ['fe9a']; // Estimote Service
+    var allowDuplicates = true;
+    noble.startScanning(serviceUUIDs, allowDuplicates, function(error) {
+      if (error) {
+        console.log('error starting scanning', error);
+      } else {
+        console.log('started scanning');
+      }
+    });
+  }
+});
+
+noble.on('discover', function(peripheral) {
+  var data = peripheral.advertisement.serviceData.find(function(el) {
+    return el.uuid == 'fe9a';
+  }).data;
+
+  var frameType = data.readUInt8(0) & 0b00001111;
+  if (frameType != ESTIMOTE_FRAME_TYPE_TELEMETRY) { return; }
+
+  var protocolVersion = data.readUInt8(0) & 0b11110000;
+  if (protocolVersion != 0) { return; }
+
+  var shortIdentifier = data.toString('hex', 1, 9);
+
+  var subFrameType = data.readUInt8(9) & 0b00000011;
+  if (subFrameType == ESTIMOTE_TELEMETRY_SUBFRAME_A) {
+    var accelerationX = data.readUInt8(10) * 2 / 127.0;
+    var accelerationY = data.readUInt8(11) * 2 / 127.0;
+    var accelerationZ = data.readUInt8(12) * 2 / 127.0;
+    var isMoving = (data.readUInt8(15) & 0b00000011) == 1;
+
+    console.log({
+      shortIdentifier: shortIdentifier,
+      acceleration: {
+        x: accelerationX,
+        y: accelerationY,
+        z: accelerationZ
+      },
+      isMoving: isMoving
+    });
+  } else if (subFrameType == ESTIMOTE_TELEMETRY_SUBFRAME_B) {
+
+  }
+});
